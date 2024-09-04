0ba36910ba17d63d4f43462bcd92ab8fb1cb1450
mosterTeco <reyesmls11@hotmail.com>
2024-09-04 15:18:29 -0600

    Actualizacion Header

--- src/Principal/Principal.jsx
+++ src/Principal/Principal.jsx
@@ -1,11 +1,19 @@
 // import {Button, TextField, Box} from "@mui/material";
 import './Principal.css'
+import rlceImage from '../Imagenes/rlce.webp'; 
+import ivaiImage from '../Imagenes/ivai.webp'
 
 function Principal() {
     return(
     <>
         <section class="layout">
-        <div class="header">1</div>
+        <div class="header">
+            <img src={rlceImage} alt="" className="header-img-right"/>
+            <h1 className="header-title">
+                    Cursos disponibles
+            </h1>
+            <img src={ivaiImage} alt="" className="header-img-left"/>
+        </div>
         <div class="main">
             <p>Parrafo</p>
         </div>
