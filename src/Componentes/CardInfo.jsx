3631c0c52e36ee516f9389804a28277f37bee69a
AngelSuarez03 <Angnusa11@gmail.com>
2024-09-05 13:34:43 -0600

    Correción stylo componente card

--- src/Componentes/CardInfo.jsx
+++ src/Componentes/CardInfo.jsx
@@ -1,19 +1,19 @@
 import { Button, Card, CardActions, CardContent, CardHeader, Typography } from "@mui/material";
-import './CardInfo.css'
+
 
 function CardInfo(Props) {
     return (
         <>
-            <Card variant="outlined" className="EstiloCard">
-                <CardHeader className="HeaderCard" title={Props.NombreCurso} />
-                <CardContent className="ContenidoCard">
+            <Card variant="outlined" sx={{ width: 750, height: 270, backgroundColor: '#A35494' }}>
+                <CardHeader sx={{ color: '#FFFFFF', marginLeft: 2 }} title={Props.NombreCurso} />
+                <CardContent sx={{ color: '#FFFFFF', marginLeft: 2 }}>
                     <Typography variant="body2">Persona que imparte el curso : {Props.ExpositorCurso}</Typography>
                     <Typography variant="body2">Lugar: {Props.LugarCurso}</Typography>
                     <Typography variant="body2">Fecha: {Props.FechaCurso}</Typography>
                     <Typography variant="body2">Hora: {Props.HoraCurso}</Typography>
                 </CardContent>
                 <CardActions>
-                    <Button variant="contained" className="Boton">{Props.TextoBoton}</Button>
+                    <Button variant="contained" sx={{ backgroundColor: '#E7B756', color: "#1E1E1E", marginLeft:3, marginBottom:3 }}>{Props.TextoBoton}</Button>
                 </CardActions>
             </Card>
         </>
